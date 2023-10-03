79272736af0c7c994fb39d9522b8cc61497f77e1
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-02 20:33:02 -0500

    Working on generate quiz button

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,37 +1,69 @@
-//Quiz (questions)
-//Front Page - Coding Quiz Challenge - what do you need to do (welcome screen)
+//Quiz (questions) - take quiz
+var generateBtn = document.querySelector("#button");
 
-// View High Scores (left hand side) - learning to do on Tuesday//
+//javascript looking for click of button to generate quiz/event
+generateBtn.addEventListener("click", function () {
+  generateQuiz()
+})
 
-// Timer (right hand side corner) (code taken from lecture - Activity 04- Stu Traverse)
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
 
-var secondsLeft = 90;
+function generateQuiz() {
+  //Front Page - Coding Quiz Challenge - what do you need to do (welcome screen)
+  var container = document.querySelector(".questions");
 
-function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
+  container.addEventListener("click", function (event) {
+    var element = event.target;
 
-    if (secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
+    if (element.matches(".ques")) {
+      var state = element.getAttribute("data-state");
+
+      // Use an if statement to conditionally render the number on the card
+      if (state === "correct") {
+        // If the card is clicked while the state is "hidden", we set .textContent to the number 
+        element.textContent = "Correct!";;
+        // Using the dataset property, we change the state to visible because the user can now see the numberß
+
+      } else {
+        // 'Hide' the number by setting .textContent to an empty string
+        element.textContent = "";
+        // Use .setAttribute() method
+        element.setAttribute("data-state", "Incorrect!")
+
+      }
     }
 
-  }, 1000);
+  });
+  // View High Scores (left hand side) - learning to do on Tuesday//
+
+  // Timer (right hand side corner) (code taken from lecture - Activity 04- Stu Traverse)
+  var timeEl = document.querySelector(".time");
+  var mainEl = document.getElementById("main");
+
+  var secondsLeft = 90;
+
+  function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+
+      if (secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+
+    }, 1000);
+  }
+
+  // Function to create and append colorsplosion image
+  function sendMessage() {
+    timeEl.textContent = " ";
+    var time = element
+    mainEl.appendChild(time);
+
+  }
+
+  setTime();
 }
-
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  var time = element
-  mainEl.appendChild(time);
-
-}
-
-setTime();
-
