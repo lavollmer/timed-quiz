33ef7d60a4fd9847521643a3e74d1164d4bf2f35
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-04 09:35:55 -0500

    redo logic behind code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,68 +1,87 @@
 // Timer (right hand side corner) (code taken from lecture - Activity 04- Stu Traverse)
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
+var startButton = document.querySelector(".btn");
 
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
 var secondsLeft = 90;
 
+//sets timer on quiz
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
-
-    if (secondsLeft === 0) {
+    if (secondsLeft >= 0) {
+      //if answer incorrect and timer count is abover zero
+      if (isIncorrect && timerCount > 0) {
+        secondsLeft--;
+      }
+    } else (secondsLeft === 0); {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      sendMessage();
+      loseGame();
     }
 
   }, 1000);
 }
 
-//Quiz (questions) - take quiz button
-var generateBtn = document.querySelector(".btn");
-//javascript looking for click of button to generate quiz/event
-generateBtn.addEventListener("click", runQuiz);
+//global variables
+var questions = ["What type of animal is a snow leopard?"]
 
-var roseColor = ["Yellow", "White", "Red", "Pink", "Violet"]
-
-
-function generateQuiz() {
-  console.log(roseColor);
-
-  //Question 1(ask the user - no functionality)
-  //Answer 1(correct) - say correct
-  //Answer 2(false) - say incorrect
-  //Answer 3(false) - say incorrect
-  //Answer 4(false) - say incorrect
+function init() {
+  getWins();
+  getlosses();
 }
-//quiz function for questions
 
-// function generateQuiz() {
-//   //Front Page - Coding Quiz Challenge - what do you need to do (welcome screen)
-//   var container = document.querySelector(".questions");
-
-//   container.addEventListener("click", function (event) {
-//     var element = event.target;
-
-//     if (element.matches(".ques")) {
-//       var state = element.getAttribute("data-state");
-
-//       // Use an if statement to conditionally render the number on the card
-//       if (state === "correct") {
-//         element.textContent = "Correct!";;
-//       } else {
-//         element.textContent = "";
-//         element.setAttribute("data-state", "Incorrect!")
-
-//       }
-//     }
-
-//   })
-// }
-
+//run quiz by generating quiz and starting timer
 function runQuiz() {
-  var quiz = generateQuiz();
-  var quizTime = setTime();
+  generateQuiz();
+  setTime();
 }
+
+//run the quiz
+function generateQuiz() {
+  isWin = false;
+  quizQuestions();
+}
+
+function quizQuestions() {
+  for (var i = 0; i < questions.length; i++) {
+    chosenQuestion = questions[i]
+  }
+}
+
+//win when condition is met
+function winCounts() {
+  winCounter++;
+  startButton.disabled = false;
+  setWins()
+}
+
+//loseGame counter
+function loseGame() {
+  loseCounter++;
+  startButton.disabled = false;
+  setLosses()
+}
+
+function getWins() {
+  var winsLocal = localStorage.getItem("winCount");
+  if (winsLocal === null) {
+    winCounter = 0;
+  } else {
+    winCounter = winsLocal;
+  }
+  winCounter.textContent = winCounter;
+}
+
+//startButton will startQuiz when clicked 
+startButton.addEventListener("click", runQuiz);
+
+//fires init when page is loaded
+init();
+
